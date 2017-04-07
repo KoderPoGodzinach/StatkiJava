e49e810107a5796df7b5ea0887358d11c376c5bd
Rafal Dziedzic <rafal-dziedzic@gazeta.pl>
2017-04-07 13:24:33 +0200

    Wypelninie planszy za pomoca tablic

--- Statki.java
+++ Statki.java
@@ -3,9 +3,25 @@ package pl.KoderPoGodzinach;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// tworzymy 10 tablic o długości 10
+        char [][] board = new char[10][10];
+        
+        // metoda która będzie wypełniała tablice 
+        
+        fillBoard(board);
+        
         printLetters();
-        printBoard();
+        printBoard(board);
+        printLetters();
+        printBoard(board);
+    }
+
+    private static void fillBoard(char[][] board) {
+        for (int i = 0; i < 10 ; i++) {
+            for (int j = 0; j <10 ; j++) {
+                board[i][j] = getRandomShip();
+            }
+        }
     }
 
     static void printLetters() {
@@ -16,14 +32,15 @@ public class Main {
         System.out.println(" ");
     }
 
-    static void printBoard() {
-        for (int i = 1; i <=10 ; i++) {
-            if(i<10) {
+    static void printBoard(char [][] board) { //mimo takiej samej nazwy są to dwie inne zmienne
+        for (int i = 0; i <10 ; i++) {      // ponieważ zmienne mają zasięgi
+            int numberToPrint  = i+1;
+            if(numberToPrint < 10) {
                 System.out.print(" ");
             }
-            System.out.print(i);
-            for (int j = 1; j <=10 ; j++) {
-                char shipValue = getRandomShip();
+            System.out.print(numberToPrint);  // wyświetlane dla 10 dodatkowa spacja
+            for (int j = 0; j < 10 ; j++) {
+                char shipValue = board[i][j]; //dostęp do poszczególnego wiersza i kolumny
                 System.out.print(shipValue);
             }
             System.out.print('\n');
@@ -37,4 +54,5 @@ public class Main {
         }
     }
 }
+
 
