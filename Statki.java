631d01715568d1fc6197673bd3e73614035569b5
Rafal Dziedzic <rafal-dziedzic@gazeta.pl>
2017-04-07 13:06:01 +0200

    Plansza za pomoca funkcji

--- Statki.java
+++ Statki.java
@@ -1,29 +1,40 @@
-// Statki 
 package pl.KoderPoGodzinach;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        printLetters();
+        printBoard();
+    }
+
+    static void printLetters() {
         System.out.print("  ");
         for (int i = 0; i <10 ; i++) {
             System.out.print((char)('A'+i));
         }
         System.out.println(" ");
+    }
+
+    static void printBoard() {
         for (int i = 1; i <=10 ; i++) {
             if(i<10) {
                 System.out.print(" ");
             }
             System.out.print(i);
             for (int j = 1; j <=10 ; j++) {
-                if(Math.random() < 0.2) {
-                    System.out.print('O');
-                } else {
-                    System.out.print('X');
-                }
+                char shipValue = getRandomShip();
+                System.out.print(shipValue);
             }
             System.out.print('\n');
-
+        }
+    }
+    private static char getRandomShip() {
+        if(Math.random() < 0.2) {
+            return 'O';
+        } else {
+            return 'X';
         }
     }
 }
+
